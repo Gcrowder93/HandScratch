4de1360cf8c3c125a6cb068f72213ef42c872f71
Chase Crowder <gcrowder93@gmail.com>
2022-03-16 16:46:15 -0700

    added basic table structure

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,26 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS discs;
+
+CREATE TABLE discs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    {/* flight(turn or fade), price(number) */}
+);
+
+CREATE TABLE drinks (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    {/* temp(hot or cold), alcoholic(yes or no) */}
+);
+
+CREATE TABLE games (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    {/* name, rating(number) */}
+);
+
+CREATE TABLE pets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    {/* name, age(number) */}
+);
+
+CREATE TABLE pokemon (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    {/* type, doesevolve(yes or no) */}
+);
