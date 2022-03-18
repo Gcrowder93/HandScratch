563205efb6e5b8e609ef0e4cb04bb4b58bb3e1ff
Chase Crowder <gcrowder93@gmail.com>
2022-03-18 15:29:03 -0700

    i believe i forgot to commit the last tests for suit 3...apologies. this is also the first test passing of suit 4

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -16,12 +16,53 @@ describe('quotable routes', () => {
   it('should be able to create a post', async () => {
     const res = await request(app)
       .post('/api/v1/games')
-      .send({ name: 'borderlands', rating: '9.5' });
+      .send({ name: 'borderlands', rating: '9' });
+  });
 
-    // expect(res.body).toEqual({
-    //   id: expect.any(String),
-    //   name: 'borderlands',
-    //   rating: '9.5',
-    // });
+  it('should be able to list games by id', async () => {
+    const game = await Games.insert({ name: 'borderlands', rating: '9' });
+    const res = await request(app).get(`/api/v1/games/${game.id}`);
+
+    expect(res.body).toEqual(game);
+  });
+
+  it('should be able to list all games', async () => {
+    const expected = await Games.insert({
+      name: 'borderlands',
+      rating: '9',
+    });
+    const res = await request(app).get('/api/v1/games');
+
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        name: 'borderlands',
+        rating: 9,
+      },
+    ]);
+  });
+
+  it('should be able to delete a game', async () => {
+    const game = await Games.insert({ name: 'borderlands', rating: '9' });
+    const res = await request(app).delete(`/api/v1/games/${game.id}`);
+
+    expect(res.body).toEqual(game);
+    expect(await Games.getById(game.id)).toBeNull();
+  });
+
+  it('should be able to update a game', async () => {
+    const game = await Games.insert({ name: 'borderlands', rating: '9' });
+    const res = await request(app)
+      .patch(`/api/v1/games/${game.id}`)
+      .send({ name: 'borderlands 2' });
+
+    const expected = {
+      id: expect.any(String),
+      name: 'borderlands 2',
+      rating: 9,
+    };
+
+    expect(res.body).toEqual(expected);
+    expect(await Games.getById(game.id)).toEqual(expected);
   });
 });
--- __tests__/pets.test.js
+++ __tests__/pets.test.js
@@ -1,15 +1,21 @@
-// const pool = require('../lib/utils/pool');
-// const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
-// const Pets = require('../lib/models/Pets');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Pets = require('../lib/models/Pets');
 
-// describe('quotable routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
+describe('quotable routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-//   afterAll(() => {
-//     pool.end();
-//   });
-// });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should be able to create a post', async () => {
+    const res = await request(app)
+      .post('/api/v1/songs')
+      .send({ name: 'sophie', age: '10' });
+  });
+});
