4f21b3ad7ec46373123aa352ae052e2aa67343e4
Chase Crowder <gcrowder93@gmail.com>
2022-03-18 16:54:07 -0700

    1st test of last table pokemon

--- __tests__/pokemon.test.js
+++ __tests__/pokemon.test.js
@@ -1,15 +1,21 @@
-// const pool = require('../lib/utils/pool');
-// const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
-// const Pokemon = require('../lib/models/Pokemon');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const Pokemon = require('../lib/models/Pokemon');
 
-// describe('quotable routes', () => {
-//   beforeEach(() => {
-//     return setup(pool);
-//   });
+describe('quotable routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-//   afterAll(() => {
-//     pool.end();
-//   });
-// });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should be able to create a post', async () => {
+    const res = await request(app)
+      .post('/api/v1/songs')
+      .send({ type: 'fire', doesEvolve: true });
+  });
+});
